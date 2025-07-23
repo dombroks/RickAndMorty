ebbc63198eedcccd1be7489800dd90a4c775f2c2
Dom Broks <tinoutech@gmail.com>
2025-07-23 16:16:19 +0100

    Setup DTOs

---
+++ app/src/main/java/com/younesbelouche/rickandmorty/data/data_sources/remote/dto/CharacterDto.kt
@@ -0,0 +1,16 @@
+package com.younesbelouche.rickandmorty.data.data_sources.remote.dto
+
+data class CharacterDto(
+    val id: Long,
+    val name: String,
+    val status: String,
+    val species: String,
+    val type: String,
+    val gender: String,
+    val originDto: OriginDto,
+    val location: LocationDto,
+    val image: String,
+    val episode: List<String>,
+    val url: String,
+    val created: String,
+)
---
+++ app/src/main/java/com/younesbelouche/rickandmorty/data/data_sources/remote/dto/LocationDto.kt
@@ -0,0 +1,6 @@
+package com.younesbelouche.rickandmorty.data.data_sources.remote.dto
+
+data class LocationDto(
+    val name: String,
+    val url: String,
+)
