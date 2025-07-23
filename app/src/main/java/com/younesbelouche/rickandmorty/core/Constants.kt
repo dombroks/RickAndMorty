290dc106a69b518c3e4674776507804d5993dbcc
Dom Broks <tinoutech@gmail.com>
2025-07-23 15:06:19 +0100

    Create constants file

---
+++ app/src/main/java/com/younesbelouche/rickandmorty/core/Constants.kt
@@ -0,0 +1,7 @@
+package com.younesbelouche.rickandmorty.core
+
+object Constants {
+    const val API_KEY = ""
+    const val PAGE_SIZE = 20
+    const val BASE_URL = "https://rickandmortyapi.com/api"
+}
