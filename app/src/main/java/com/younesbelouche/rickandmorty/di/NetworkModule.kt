a4a1c0c8f96a427d4edd54c71afee009b8dcc6a0
Dom Broks <tinoutech@gmail.com>
2025-07-24 18:45:06 +0100

    Implement Network, Repository and UseCase modules

--- app/src/main/java/com/younesbelouche/rickandmorty/di/NetworkModule.kt
+++ app/src/main/java/com/younesbelouche/rickandmorty/di/NetworkModule.kt
@@ -1,4 +1,66 @@
 package com.younesbelouche.rickandmorty.di
 
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.younesbelouche.rickandmorty.core.Constants
+import com.younesbelouche.rickandmorty.data.remote.CharactersApi
+import com.younesbelouche.rickandmorty.data.remote.interceptor.CacheInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideCacheInterceptor() = CacheInterceptor()
+
+    @Singleton
+    @Provides
+    fun provideOkHttp(
+        @ApplicationContext context: Context,
+        cacheInterceptor: CacheInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder().apply {
+            cache(Cache(File(context.cacheDir, "http-cache"), 10L * 1024L * 1024L))
+            addNetworkInterceptor(cacheInterceptor)
+            connectTimeout(3, TimeUnit.SECONDS)
+            readTimeout(20, TimeUnit.SECONDS)
+            writeTimeout(25, TimeUnit.SECONDS)
+        }.build()
+
+    }
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        return GsonBuilder().serializeNulls().setLenient().create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient, gson: Gson): Retrofit = Retrofit
+        .Builder()
+        .baseUrl(Constants.BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideCharactersApi(retrofit: Retrofit): CharactersApi =
+        retrofit.create(CharactersApi::class.java)
 }
--- app/src/main/java/com/younesbelouche/rickandmorty/di/RepositoryModule.kt
+++ app/src/main/java/com/younesbelouche/rickandmorty/di/RepositoryModule.kt
@@ -1,4 +1,24 @@
 package com.younesbelouche.rickandmorty.di
 
+import com.younesbelouche.rickandmorty.data.remote.CharactersApi
+import com.younesbelouche.rickandmorty.data.repositories.CharactersRepositoryImpl
+import com.younesbelouche.rickandmorty.domain.repositories.CharactersRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
 object RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideNewsRepository(
+        charactersApi: CharactersApi
+    ): CharactersRepository {
+        return CharactersRepositoryImpl(charactersApi = charactersApi)
+    }
 }
