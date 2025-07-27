f4ce00a537cc2da0c8e667d04f7c90a2487c3a54
Dom Broks <tinoutech@gmail.com>
2025-07-27 20:17:16 +0100

    Optimize imports

--- app/src/main/java/com/younesbelouche/rickandmorty/presentation/characterdetails/CharacterDetailsEvent.kt
+++ app/src/main/java/com/younesbelouche/rickandmorty/presentation/characterdetails/CharacterDetailsEvent.kt
@@ -1,7 +1,5 @@
 package com.younesbelouche.rickandmorty.presentation.characterdetails
 
-import com.younesbelouche.rickandmorty.presentation.characters.CharactersEvent
-
 sealed class CharacterDetailsEvent {
     data class GetCharacter(val characterId: Int) : CharacterDetailsEvent()
 }
--- app/src/main/java/com/younesbelouche/rickandmorty/presentation/characters/CharactersViewModel.kt
+++ app/src/main/java/com/younesbelouche/rickandmorty/presentation/characters/CharactersViewModel.kt
@@ -1,21 +1,14 @@
 package com.younesbelouche.rickandmorty.presentation.characters
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import androidx.paging.map
 import com.younesbelouche.rickandmorty.domain.usecases.GetCharactersUseCase
 import com.younesbelouche.rickandmorty.domain.usecases.SearchCharacterUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
